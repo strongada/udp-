8c258e2b1d12ea4fc726856c66b7c33b21400edd
cuizhewei <18120046@bjtu.edu.cn>
2018-11-24 17:23:48 +0800

    客户端

--- udp_client.py
+++ udp_client.py
@@ -3,9 +3,5 @@ import time
 address = ("192.168.100.133",31500)
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 msg = "this is test message"
-start = time.time()
 s.sendto(msg,address)
 s.close()
-end = time.time()
-print end-start
-
